b5e66037c817da93e7c5e2ccb14c9fd40f2a49e9
jogabi <rkql1109@naver.com>
2021-12-15 11:53:17 +0900

    영화상세정보 축 ㅏ

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
-import { API_URL, API_KEY } from '../../Config'
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config'
+import MainImage from '../LandingPage/Sections/MainImage';
+import MovieInfo from './Sections/MovieInfo';
 
 function MovieDetail( props ) {
   const id = props.match.params.id
-  useEffect( () => {
-    const endpoint = `${API_URL}movie/${id}?api_key=${API_KEY}&language=en-US`
 
-    fetch( endpoint )
+  const [Movie, setMovie] = useState( [] )
+
+  useEffect( () => {
+    const endpointCrew = `${API_URL}movie/${id}?api_key=${API_KEY}&language=en-US`
+    const endpointInfo = `${API_URL}movie/${id}?api_key=${API_KEY}&language=en-US`
+
+    fetch( endpointInfo )
       .then( response => response.json() )
       .then( response => {
-
-        console.log( response );
+        setMovie( response )
       } )
 
 
   }, [] )
   return (
     <div>
-      test
+      {/* header */}
+      <MainImage
+        image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`} title={Movie.original_title} description={Movie.overview}
+      ></MainImage>
+
+      <MovieInfo movie={Movie}></MovieInfo>
+
+      {/* body */}
+      <div style={{ width: '85%', margin: '1rem auto' }}>
+        <br />
+        {/* actore grid */}
+        <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
+          <button>Toggle Actor View</button>
+        </div>
+
+
+      </div>
+
     </div>
   )
 }
